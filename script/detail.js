59d85b60e281afa1db4a012f4c8e1526a4882abe
rafikusdiarto <rafikusdiarto1707@gmail.com>
2022-10-23 16:06:49 +0700

    detailPartner

--- script/detail.js
+++ script/detail.js
@@ -11,15 +11,11 @@ const getDataApi = () => {
       // let dataPartner = result.results;
       result.forEach((data) => {
           partner.innerHTML +=
-            `<div class="row pt-5 pb-4">
+            `<div class="row my-4 p-3">
                 <div class="col-md-3">
                     <img src="https://picsum.photos/200/300?random=1" class="rounded" />
                     <div class="ratings pt-2">
-                    <i class="fa fa-star rating-color"></i>
-                    <i class="fa fa-star rating-color"></i>
-                    <i class="fa fa-star rating-color"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
+                    <p class=""><b>`+data.rating+`</b></p>
                     </div>
                 </div>
                 <div class="col-md-9">
@@ -36,6 +32,7 @@ const getDataApi = () => {
                     <a href="https://api.whatsapp.com/send/?phone=087809680106&text&type=phone_number&app_absent=0" target="_blank" class="btn btn-warning rounded-5" tabindex="-1" role="button" aria-disabled="true" ><i class="fab fa-whatsapp"></i> Contact Now</a>
                 </div>
             </div> 
+            
             <footer class="text white pt-5 pb-4 gradient-background">
             <div class="container review">
               <h3><b>Reviews</b></h3>
