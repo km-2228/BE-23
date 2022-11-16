540c315a6ce5cde2edca6f4424b79c8943fb6211
rafikusdiarto <rafikusdiarto1707@gmail.com>
2022-11-16 19:08:02 +0700

    revisi

--- script/login.js
+++ script/login.js
@@ -1,10 +1,11 @@
 const URL = 'https://634edef8df22c2af7b452cc0.mockapi.io/users';
 const inputEmail = document.getElementById("inputEmail");
 const inputPassword = document.getElementById("inputPassword");
-
+let user = null;
 
 // fungsi login
 function login() {
+    checkUserInWebStorage()
     const setEmail = inputEmail.value
     const setPassword = inputPassword.value;
 
@@ -13,19 +14,21 @@ function login() {
     .then(dataUser => {
         dataUser.forEach((data) => {
             if(data.email == setEmail && data.password == setPassword){
-                localStorage.setItem('loginAja',"benar");
-                localStorage.setItem('dataUser',JSON.stringify(dataUser));
+                var myJSON = JSON.stringify(data)
+                localStorage.setItem( 'dataUser', myJSON );                
+                console.log(data)
                 alert("Success Login")
                 window.location.href = "./../index.html";
             }
-            // else{
-            //     alert("invalid login")
-            //     location.reload()  
-            // }
         })
     })
     .catch((error) => console.log(error));
 }
-// login()
 
-  
+function checkUserInWebStorage(){
+    if(user !== ''){
+        user = JSON.parse(localStorage.getItem('dataUser'));
+    } else{
+        user = JSON.parse(sessionStorage.getItem('dataUser'));
+    }
+}
